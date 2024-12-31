8cd20fae6f2f7e6e81668f8c831a87dc6055c656
Enzo Bonansea <EBonansea@Eventellect.com>
2024-12-31 11:01:06 -0300

    feat(alloc.c): printing pointer address

--- alloc.c
+++ alloc.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
+#include "stdio.h"
 
 int main() {
-    int qty = 1000;
-    float* f = (float*)malloc(qty * sizeof(float));
-    for (int i = 0; i < qty; i++) {
-        f[i] = i + 0.5f;
-    }
-    free(f);
+    int qty = 10000;
+    float* ptr_f = (float*)malloc(qty*sizeof(float));
+    printf("ptr_f=%p\n", ptr_f);
+    for (int i=0; i<qty; i++) *(ptr_f+i) = 0.6f;
+    free(ptr_f);
 
     return 0;
 }
