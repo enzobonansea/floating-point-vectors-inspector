6a776308a2adb2cd6bd58c867df8acb1a85d7696
Enzo Bonansea <EBonansea@Eventellect.com>
2025-01-03 21:07:01 -0300

    feat(menu.sh): test alloc.c option

--- menu.sh
+++ menu.sh
@@ -5,30 +5,31 @@ while true; do
     echo "1. Test blender"
     echo "2. Test SPEC2017"
     echo "3. Run bash"
-    echo "4. Exit"
+    echo "4. Test alloc.c"
+    echo "5. Exit"
     read -p "Enter your choice (1-4): " choice
 
     case $choice in
         1)
-            echo "Running Valgrind on Blender..."
-            inst/bin/valgrind --tool=memcheck --undef-value-errors=no blender -b -noaudio -P /tmp/example.py
+            echo "Testing Blender..."
+            inst/bin/valgrind --tool=memcheck --undef-value-errors=no blender -b -noaudio -P /tmp/example.py > /tmp/stdout.log 2>&1
             
             echo ""
             echo ""
-            echo "Move /tmp/malloc.log and /tmp/heap_write.log to your local machine and analyze them following results\blender\test1.ipynb"
+            echo "Analyze /tmp/stdout.log"
             /bin/bash
             ;;
         2)
             read -p "Which SPEC2017 app do you want to test? " user_input
-            echo "Running Valgrind on the SPEC2017 app $user_input"
+            echo "Testing the SPEC2017 app $user_input"
             cd /usr/cpu2017
             source shrc
             cd /opt/valgrind
-            inst/bin/valgrind --tool=memcheck --undef-value-errors=no runcpu "$user_input"
+            inst/bin/valgrind --tool=memcheck --undef-value-errors=no runcpu "$user_input"  > /tmp/stdout.log 2>&1
 
             echo ""
             echo ""
-            echo "Move /tmp/malloc.log and /tmp/heap_write.log to your local machine and analyze them following results\blender\test1.ipynb"
+            echo "Analyze /tmp/stdout.log"
             /bin/bash
             ;;
         3)
@@ -36,6 +37,12 @@ while true; do
             exit 0
             ;;
         4)
+            echo "Testing alloc.c ..."
+            inst/bin/valgrind --tool=memcheck /tmp/alloc  > /tmp/stdout.log 2>&1
+            echo "Analyze /tmp/stdout.log"
+            /bin/bash
+            ;;
+        5)
             echo "Exiting..."
             exit 0
             ;;
@@ -44,3 +51,5 @@ while true; do
             ;;
     esac
 done
+
+# cd /usr/cpu2017/benchspec/CPU/519.lbm_r/src
