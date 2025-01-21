faad909a20e626700f9e7673e16951d3aa153344
Kevin Frachtenberg <8524153+kevinfra@users.noreply.github.com>
2025-01-21 14:07:13 -0300

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -3,12 +3,11 @@
 while true; do
     echo "Select an option:"
     echo "1. Test alloc.c"
-    echo "2. Test lbm avoiding runspec"
-    echo "3. Test namd avoiding runspec"
-    echo "4. Test bwaves avoiding runspec"
-    echo "5. Run bash"
-    echo "6. Test alloc.c"
-    echo "7. Exit"
+    echo "2. Run bash"
+    echo "3. Exit"
+    echo "4. Test lbm avoiding runspec"
+    echo "5. Test namd avoiding runspec"
+    echo "6. Test bwaves avoiding runspec"
 
     read -p "Enter your choice: " choice
 
@@ -20,30 +19,24 @@ while true; do
             /bin/bash
             ;;
         2)
-            /usr/local/bin/spec/lbm.sh
-            /bin/bash
-            ;;
-        3)
-            /usr/local/bin/spec/namd.sh
-            /bin/bash
-            ;;
-        4)
-            /usr/local/bin/spec/bwaves.sh
-            /bin/bash
-            ;;
-        5)
             bash
             exit 0
             ;;
-        6)
-            echo "Testing alloc.c ..."
-            inst/bin/valgrind --tool=memcheck --log-file=/tmp/memlog.log --undef-value-errors=no -- /tmp/alloc
-            echo "Analyze /tmp/memlog.log"
-            /bin/bash
-            ;;
-        7)
+        3)
             echo "Exiting..."
             exit 0
+            ;;
+        4)
+            /usr/local/bin/spec/lbm.sh
+            /bin/bash
+            ;;
+        5)
+            /usr/local/bin/spec/namd.sh
+            /bin/bash
+            ;;
+        6)
+            /usr/local/bin/spec/bwaves.sh
+            /bin/bash
             ;;
         *)
             echo "Invalid option."
