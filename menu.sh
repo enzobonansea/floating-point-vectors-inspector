932059a9147a5ca108fba5a60db73eea60594bab
Enzo Bonansea <EBonansea@Eventellect.com>
2025-01-21 11:23:27 +0100

    feat(menu.sh): alloc.c is now the first option and run bash the last one

--- menu.sh
+++ menu.sh
@@ -2,31 +2,31 @@
 
 while true; do
     echo "Select an option:"
-    echo "1. Test lbm avoiding runspec"
-    echo "2. Test namd avoiding runspec"
-    echo "3. Run bash"
-    echo "4. Test alloc.c"
+    echo "1. Test alloc.c"
+    echo "2. Test lbm avoiding runspec"
+    echo "3. Test namd avoiding runspec"
+    echo "4. Run bash"
     echo "5. Exit"
     read -p "Enter your choice: " choice
 
     case $choice in
         1)
-            /usr/local/bin/spec/lbm.sh
-            /bin/bash
-            ;;
-        2)
-            /usr/local/bin/spec/namd.sh
-            /bin/bash
-            ;;
-        3)
-            bash
-            exit 0
-            ;;
-        4)
             echo "Testing alloc.c ..."
             inst/bin/valgrind --tool=memcheck --log-file=/tmp/memlog.log --undef-value-errors=no -- /usr/alloc
             echo "Analyze /tmp/memlog.log"
             /bin/bash
+            ;;
+        2)
+            /usr/local/bin/spec/lbm.sh
+            /bin/bash
+            ;;
+        3)
+            /usr/local/bin/spec/namd.sh
+            /bin/bash
+            ;;
+        4)
+            bash
+            exit 0
             ;;
         5)
             echo "Exiting..."
