d1f909ae1dabb4f61248c1b1748ab904e9c7eaaa
Enzo Bonansea <EBonansea@Eventellect.com>
2025-01-22 14:18:16 +0100

    feat(alloc.c): buffer is now bigger

--- alloc.c
+++ alloc.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 
 int main() {
-    int qty = 10000;
+    int qty = 10000000;
     float* ptr_f = (float*)malloc(qty*sizeof(float));
     printf("ptr_f=%p\n", ptr_f);
     for (int i=0; i<qty; i++) *(ptr_f+i) = 0.6f;
