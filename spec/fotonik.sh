79e8fe8a16f1a6951da373492fdc04d3d3c667d9
Enzo Bonansea <EBonansea@Eventellect.com>
2025-05-04 17:26:06 +0200

    fix: correct file path in runcpu command for consistency

--- spec/fotonik.sh
+++ spec/fotonik.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd /usr/cpu2017
 . ./shrc
-runcpu --action=build --config=./enzo.cfg --size=test 549.fotonik3d
-/opt/valgrind/inst/bin/valgrind --tool=memcheck --leak-check=no --track-origins=no --log-file=/tmp/memlog.log --undef-value-errors=no --trace-children=yes -- runcpu --action=run --config=enzo.cfg --size=test --nobuild 549.fotonik3d
+runcpu --action=build --config=enzo.cfg --size=test 549.fotonik3d
+runcpu --action=run --config=enzo.cfg --size=test --nobuild 549.fotonik3d
