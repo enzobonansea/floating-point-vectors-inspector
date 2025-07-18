a813abffc0cdebb7a7d9f881651d5e038a8954ae
Enzo Bonansea <EBonansea@Eventellect.com>
2025-07-18 07:50:10 +0200

    fix: Simplify runcpu command in fprate.sh by removing unnecessary build action

--- spec/fprate.sh
+++ spec/fprate.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 cd /usr/cpu2017
 . ./shrc
-runcpu --action=build --config=memlog-monitor.cfg --size=test fprate
-runcpu --action=run --config=memlog-monitor.cfg --size=test --nobuild fprate
+runcpu --action=run --config=memlog-monitor.cfg --size=test fprate
