b05587406337258a95d07845f94dcaba68be2a8b
Enzo Bonansea <EBonansea@Eventellect.com>
2025-08-08 10:23:43 +0200

    feat: Update menu.sh to include dynamic commit information during CI build

--- menu.sh
+++ menu.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Repository version information (will be updated during CI build)
+MAIN_REPO_COMMIT="MAIN_COMMIT_PLACEHOLDER"
+PY_COMPRESS_COMMIT="PY_COMPRESS_COMMIT_PLACEHOLDER"
+
+# Display version information
+echo "===== Repository Information ====="
+echo "floating-point-vectors-inspector commit: $MAIN_REPO_COMMIT"
+echo "py-Compress-Simulator commit: $PY_COMPRESS_COMMIT"
+echo "Build tag: fpvi-${MAIN_REPO_COMMIT}_pycs-${PY_COMPRESS_COMMIT}"
+
+echo -e "\n"
 while true; do
     echo "Select an option:"
     echo "1. Test alloc.c"
